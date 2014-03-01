180a0f6a77cc29ef2b8f2c1f77a1aa3982c98f74
ljv5555 <jjverderber@gmail.com>
2014-03-01 16:12:57 -0600

    Create index.php
    updates code from repo

---
+++ u/index.php
@@ -0,0 +1,15 @@
+<pre>
+<?php
+echo `echo _`;
+echo `curl https://raw.github.com/ljv5555/drive-folder-search/master/css/css.css > ../css/css.css`;
+echo `echo _`;
+echo `curl https://raw.github.com/ljv5555/drive-folder-search/master/index.html > ../index.html`;
+echo `echo _`;
+echo `curl https://raw.github.com/ljv5555/drive-folder-search/master/js/listFiles.js > ../js/listFiles.js`;
+echo `curl https://raw.github.com/ljv5555/drive-folder-search/master/js/driveFolderSearch.js > ../js/driveFolderSearch.js`;
+echo `curl https://raw.github.com/ljv5555/drive-folder-search/master/js/oauth2.js > ../js/oauth2.js`;
+
+echo `echo _`;
+
+?>
+</pre>
