c3b893f46fb03f95000dc404fb2099bf42f52471
ljv5555 <jjverderber@gmail.com>
2014-03-23 15:05:05 -0500

    updating code

---
+++ app/js/driveFolderSearch.js
@@ -0,0 +1,17 @@
+/*
+
+window['googleDriveClient']=null;
+
+var googleDriveClient = null;
+var driveClientInit = function()
+{
+  jQuery('.listFoldersResult').slideDown();
+  googleDriveClient = new GoogleDriveClient();
+  googleDriveClient.setToken(gapi.auth.getToken().access_token);
+  jQuery('.listFoldersResult').slideUp();
+};
+
+*/
+// var lastcb = function(r){jQuery(r).html('<pre>'+JSON.stringify(r)+'</pre>');};
+		
+		
